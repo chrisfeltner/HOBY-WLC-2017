2722446ec9c7f08557ab9e6615eaf1c2e814dce2
Christopher Feltner <chris.feltner@knights.ucf.edu>
2017-05-12 20:12:54 -0400

    created Event class

---
+++ HOBY WLC 2017/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  HOBY WLC 2017
+//
+//  Created by Christopher Feltner on 5/12/17.
+//  Copyright © 2017 Christopher Feltner. All rights reserved.
+//
+
+import Foundation
+
+class Event
+{
+    var name = ""
+    var startTime = ""
+    var endTime = ""
+}
--- HOBY WLC 2017/ScheduleViewController.swift
+++ HOBY WLC 2017/ScheduleViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ScheduleViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
